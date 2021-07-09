d8d76f00bac2b533762dafd79e9de09d3c81764c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-09 20:44:31 +0530

    add a funtion App() along with export default keyword

--- App.js
+++ App.js
@@ -2,3 +2,7 @@ import * as React from 'react';
 import {Text, View, StyleSheet } from 'react-native';
 import {Card} from 'react-native-paper';
 import UpdateName from './components/UpdateName';
+
+export default function App() {
+  
+}
