6653f3f96b39ff369de7a06f56912e0b24bd4907
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-09 22:16:52 +0530

    add a Text tag inside the View tag of the funtion UpdateName()

--- components/UpdateName.js
+++ components/UpdateName.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function UpdateName() {
     return (
         <View style = {styles.container}>
-
+            <Text></Text>
         </View>
     );
 }
