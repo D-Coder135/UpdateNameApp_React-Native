cb6339bf5ff96c6a5376d6adbc239d647e139eca
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-10 19:55:50 +0530

    call the setEnteredName() function inside the updateNameOnPress() function

--- components/UpdateName.js
+++ components/UpdateName.js
@@ -20,6 +20,7 @@ export default function UpdateName() {
     const updateNameOnPress = () => {
         updatePreviousName(name);
         updateName(enteredName);
+        setEnteredName("");
     }
 
     return (
